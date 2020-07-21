abab61b5f22876a1ec23c98a84b267a3ab63ed96
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-21 09:56:25 +0530

    Backend to add and retrieve user images complete

---
+++ backend/utils/APIResponseHandler/APIResponseHandler.js
@@ -0,0 +1,21 @@
+let ResponseCodes = require("./HttpResponseCodes");
+let HTTPResponseCodes = new ResponseCodes();
+
+class APIResponseHandler {
+  handle(response, result, type) {
+    let successStatus = 200;
+    if (type === 'POST') {
+      successStatus = 201;
+      result = result._doc
+    }
+    if (!result.errors) {
+      return response.status(successStatus).send(result);
+    } else {
+      return response
+        .status(HTTPResponseCodes.BAD_REQUEST())
+        .send(result.errors);
+    }
+  }
+}
+
+module.exports = APIResponseHandler;
