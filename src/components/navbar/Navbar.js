660e861345e4faf7c4ba2f8b0ea84158d8d7d945
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-21 06:30:30 +0530

    Initial commit with authentication using aws cognito

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import withContext from "../../withContext";
+
+class Navbar extends Component {
+  componentWillMount() {
+    this.setState({
+      user: this.props.context.user,
+    });
+  }
+
+  render() {
+    return (
+      <nav
+        className="navbar container"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <b className="navbar-item is-size-4 ">E-Commerce</b>
+        </div>
+        <div className={`navbar-menu`}>
+          <Link to="/" className="navbar-item">
+            User
+          </Link>
+          {!this.props.context.user ? (
+            <>
+              {/* login and register should only show when user is not logged in */}
+              <Link to="/login" className="navbar-item">
+                Login
+              </Link>
+              <Link to="/register" className="navbar-item">
+                Register
+              </Link>
+            </>
+          ) : (
+              // Logout should only show when user is logged in
+              <a
+                href="/"
+                className="navbar-item"
+                onClick={this.props.context.logout}
+              >
+                Logout
+              </a>
+            )}
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default withContext(Navbar);
